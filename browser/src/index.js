edbe67170c4695aa8bc6fe0cf9be3b1e688f48d6
Frédéric Gascon <frederic.gascon@gmail.com>
2015-09-10 18:50:28 +0000

    major rework

--- browser/src/index.js
+++ browser/src/index.js
@@ -1,102 +1,68 @@
-var io = require('socket.io-client');
-var Promise = require('bluebird');
 var EventEmitter = require('events').EventEmitter;
-var slice = [].slice;
+var util = require('util');
 
-function createPromise(fnc){
-	return new Promise(function(resolve, reject){
-		fnc(function(err, result){
-			if(err){
-				reject(new Error(err));
-			}else{
-				resolve(result);
-			}
-		});
-	});
+function Client(){
+	EventEmitter.call(this);
+	this.logged = false;
+	this.buffer = [];
 }
 
-function wrapSocket(socket){
-	var socksub = {};
-	var events = new EventEmitter();
-	var subscriptions = {};
-	
-	socket.on('error', function(err){
-		events.emit('error', err);
-	});
-	
-	socket.on('event', function(topic, args){
-		args.unshift(topic);
-		events.emit.apply(events, args);
-	});
-	
-	socket.on('reconnect', function(){
-		for(var topic in subscriptions){
-			socket.emit('subscribe', topic, function(err){
-				if(err){
-					socket.emit('error', err);
-				}
-			});
-		}
-	});
-	
-	socksub.subscribe = function(topic, callback){
-		if(!subscriptions[topic]){
-			subscriptions[topic] = createPromise(function(callback){
-				socket.emit('subscribe', topic, callback);
-			})['catch'](function(err){
-				delete subscriptions[topic];
-				throw err;
-			});
-		}
-		return subscriptions[topic];
-	};
-	
-	socksub.unsubscribe = function(topic, callback){
-		var subscribePromise = subscriptions[topic];
-		delete subscriptions[topic];
-		return createPromise(function(callback){
-			socket.emit('unsubscribe', topic, callback);
-		})['catch'](function(err){
-			subscriptions[topic] = subscribePromise;
-			throw err;
-		});
-	};
-	
-	socksub.on = function(topic, handler, autoSubscribe){
-		events.on(topic, handler);
-		if(autoSubscribe){
-			return socksub.subscribe(topic);
-		}
-		return socksub;
-	};
-	
-	socksub.off = function(topic, handler, autoUnsubscribe){
-		if(handler){
-			events.removeListener(topic, handler);
+util.inherits(Client, EventEmitter);
+module.exports = Client;
+var proto = Client.prototype;
+
+proto.action = function action(payload, callback){
+	if(this.logged){
+		this.emit('action', payload, callback);
+	}else{
+		this.buffer.push([payload, callback]);
+	}
+};
+
+proto.flush = function flush(){
+	if(!this.logged){
+		return;
+	}
+	var buffer = this.buffer;
+	this.buffer = [];
+	for(var i = 0; i < buffer.length; i++){
+		this.emit('action', buffer[i][0], buffer[i][1]);
+	}
+};
+
+proto.login = function login(credentials){
+	var self = this;
+	this.emit('action', {
+		type: 'login',
+		cred: credentials
+	}, function(err){
+		if(err){
+			self.emit('error', err);
 		}else{
-			events.removeAllListeners(topic);
+			self.logged = true;
+			self.flush();
 		}
-		if(autoUnsubscribe){
-			if(EventEmitter.listenerCount(events, topic) === 0){
-				return socksub.unsubscribe(topic);
-			}else{
-				return Promise.resolve();
-			}
-		}
-		return socksub;
-	};
-	
-	socksub.rpc = function(method){
-		var args = slice.call(arguments, 1);
-		return createPromise(function(callback){
-			socket.emit('rpc', method, args, callback);
-		});
-	};
-	
-	return socksub;
-}
+	});
+};
 
-module.exports = function(){
-	var socket = io.connect.apply(io, arguments);
-	return wrapSocket(socket);
+proto.request = function request(method, args, callback){
+	this.action({
+		type: 'req',
+		method: method,
+		args: args
+	}, callback);
+};
+
+proto.subscribe = function subscribe(topic, callback){
+	this.action({
+		type: 'sub',
+		topic: topic
+	}, callback);
+};
+
+proto.unsubscribe = function unsubscribe(topic, callback){
+	this.action({
+		type: 'unsub',
+		topic: topic
+	}, callback);
 };
